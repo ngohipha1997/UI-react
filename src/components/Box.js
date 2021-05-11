f84d287972d0669df046fa07c3498d4dac016f6a
Ngo Hieu Phat <quadeptrai77@Ngos-MacBook-Pro.local>
2021-05-11 19:16:43 +0700

    xu ly su kien box

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,11 +3,17 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class Box extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            count : 0,
+        };
+    }
   render() {
     return (
       <View
         style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
-        <Text style={{alignSelf: 'center', fontSize: 30}}> Count : 0 </Text>
+        <Text style={{alignSelf: 'center', fontSize: 30}}> Count : {this.state.count} </Text>
         <View
           style={{
             flexDirection: 'row',
@@ -15,6 +21,11 @@ export default class Box extends Component {
             marginTop: 20,
           }}>
           <TouchableOpacity
+          onPress={() =>{
+              this.setState({count : this.state.count + 1 } , () => {
+                  console.log(this.state.count)
+              });
+          }}
             style={{padding: 10, backgroundColor: 'green', borderRadius: 5}}>
             <Text style={{fontSize: 20, color: 'white'}}>Increment</Text>
           </TouchableOpacity>
