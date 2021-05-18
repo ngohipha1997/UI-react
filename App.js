6a5aa9a9eda6cb3194319a56e8c66c91a7d79ed2
Ngo Hieu Phat <quadeptrai77@Ngos-MacBook-Pro.local>
2021-05-18 19:12:57 +0700

    ui word

--- App.js
+++ App.js
@@ -3,16 +3,18 @@ import { SafeAreaView, Text, View } from 'react-native';
 import Box from './src/components/Box';
 import Filter from './src/components/Filter';
 import From from './src/components/From';
+import Word from './src/components/Word';
 import Main from './src/screens/Main';
 
 export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={{flex : 1 }}> 
-        <Main />
+        {/* <Main /> */}
         {/* <Box/> */}
         {/* <From /> */}
         {/* <Filter/> */}
+        <Word/>
       </SafeAreaView>
     );
   }
